e788358c49c0551560c90b493de654670d610b4d
danbiton <166488125+danbiton@users.noreply.github.com>
2025-04-20 11:44:01 +0300

    Update UseSuggestions.jsx

--- src/components/hooks/UseSuggestions.jsx
+++ src/components/hooks/UseSuggestions.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { debounce } from "../../lib/Index";
+import { debounce } from "../../lib/index";
 
 const UseSuggestions = (url) => {
   const [searchInput, setSearchInput] = useState("");

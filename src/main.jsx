e85a1666cab56400304a92c3972db73f3916686a
Daniel <biton123654@gmail.com>
2025-04-22 12:08:36 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -23,5 +23,5 @@ createRoot(document.getElementById("root")).render(
   </QueryClientProvider>
 );
 
-axios.defaults.baseURL="https://construction-server-on2o.onrender.com"
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
